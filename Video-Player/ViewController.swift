d94281f6369b32667a3789e99c9a236593958981
Abhinandan Singh <singhabhinandan614@gmail.com>
2023-08-09 14:00:18 +0530

    Added Video Player Controls using AVPlayer

--- Video-Player/ViewController.swift
+++ Video-Player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit // interface for media player
+import AVFoundation // controls like.. device camera, audio...
 
 class ViewController: UIViewController {
 
@@ -13,13 +15,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    private func findVideo(){
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
+    private func playVideo(){
         guard let path = Bundle.main.path(forResource: "oogway", ofType: "mp4") else { // guard
             debugPrint("oogway.mp4 not found")
             
             return
         }
+        let player = AVPlayer(url:
+                                URL(fileURLWithPath: path))
+        let playerController =
+        AVPlayerViewController()
+        
+        playerController.player=player
+        present(playerController, animated: true){
+            player.play()
+        }
     }
 }
-
