acff2f1d0a58cafcbeaf511c4ba042e8077f56cd
Abhinandan Singh <singhabhinandan614@gmail.com>
2023-08-09 10:33:08 +0530

    Initial Commit

---
+++ Video-Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video-Player
+//
+//  Created by Abhinandan Singh on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
